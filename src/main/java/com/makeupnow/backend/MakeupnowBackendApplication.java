139db0453ecc42edb257d84df068794b3636140e
steo <nguyenstephanie@live.fr>
2025-04-19 20:09:09 +0200

    🎉 Initial commit - clean reset

---
+++ src/main/java/com/makeupnow/backend/MakeupnowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.makeupnow.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeupnowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeupnowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makeupnow/backend/MakeupnowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeupnow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeupnowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
